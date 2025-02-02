d866afe8dd656a158dfbf2406431d9aaf4db4f5e
Gustavo Dezan <gustavol.dezan@gmail.com>
2025-02-02 16:43:48 -0300

    fix 3

--- submission/003.sh
+++ submission/003.sh
@@ -4,5 +4,18 @@
 
 bhash=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_230 -rpcpassword=DzFoWDIBKWcq getblockhash 123456)
 blockdata=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_230 -rpcpassword=DzFoWDIBKWcq getblock $bhash)
-result=$(echo "$blockdata" | jq '.nTx')
-echo $result
+# echo "$blockdata" | jq -r '.nTx'
+
+txids=$(echo "$blockdata" | jq -r '.tx[]')
+
+total_outputs=0
+
+for txid in $txids; do
+    txdata=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_230 -rpcpassword=DzFoWDIBKWcq getrawtransaction "$txid" 1)
+
+    num_outputs=$(echo "$txdata" | jq '.vout | length')
+
+    total_outputs=$((total_outputs + num_outputs))
+done
+
+echo $total_outputs
