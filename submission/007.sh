e9549bca1c1177ef747cb45bcd6f86c171f9ca4d
Gustavo Dezan <gustavol.dezan@gmail.com>
2025-02-02 19:28:24 -0300

    fix 7

--- submission/007.sh
+++ submission/007.sh
@@ -2,18 +2,15 @@
 
 blockhash=$(bitcoin-cli getblockhash 123321)
 txids=$(bitcoin-cli getblock "$blockhash" | jq -r '.tx[]')
-
 for txid in $txids; do
     raw_tx=$(bitcoin-cli getrawtransaction "$txid" true)
     vout_count=$(echo "$raw_tx" | jq '.vout | length')
-
     for ((i=0; i<vout_count; i++)); do
-        scriptPubKey=$(echo "$raw_tx" | jq -r ".vout[$i].scriptPubKey")
+        scriptPubKey=$(echo "$raw_tx" | jq ".vout[$i].scriptPubKey")
         address=$(echo "$scriptPubKey" | jq -r '.address // empty')
         is_unspent=$(bitcoin-cli gettxout "$txid" "$i")
-
-        if [ "$is_unspent" != "null" ] && [ -n "$address" ]; then
-            echo "$address"
+        if [[ -n "$is_unspent" && -n "$address" ]]; then
+            echo $address
             exit 0
         fi
     done
