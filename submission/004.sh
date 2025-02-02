52c88e9818e187cc9d854e082cf1c334a0cfceec
Gustavo Dezan <gustavol.dezan@gmail.com>
2025-02-02 17:09:17 -0300

    fix 4

--- submission/004.sh
+++ submission/004.sh
@@ -2,5 +2,5 @@
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
 info=$(bitcoin-cli getdescriptorinfo "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/0/*)")
 descriptor=$(echo "$info" | jq -r '.descriptor')
-derived_taproot_address=$(bitcoin-cli deriveaddresses $descriptor "[99,99]" | jq -r '.[0]')
-echo $derived_taproot_address
+taproot_address=$(bitcoin-cli deriveaddresses "$descriptor" "[100,100]" | jq '.[0]')
+echo "$taproot_address"
