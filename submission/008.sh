5ac725859ae7a3e5ec68e01dd1f533bd935cfc36
Gustavo Dezan <gustavol.dezan@gmail.com>
2025-02-02 18:41:48 -0300

    fix 4 tentando passar o p2sh

--- submission/008.sh
+++ submission/008.sh
@@ -2,4 +2,4 @@
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
 raw=$(bitcoin-cli getrawtransaction e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163 true)
 pubkey_hex=$(echo $raw | jq -r '.vin[0].txinwitness[-1]')
-bitcoin-cli decodescript $pubkey_hex | jq -r '.segwit.address'
+bitcoin-cli decodescript $pubkey_hex | jq -r '.p2sh'
